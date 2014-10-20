968c841a5efe5cedc3925f74a85a005bf538f248
陈钢 <yikuyiku.com@gmail.com>
2014-10-20 03:30:19 +0000

    Import of CHENGANG/Redis-AOF-Tail-File-0.02 from CPAN.
    gitpan-cpan-distribution: Redis-AOF-Tail-File
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         CHENGANG/Redis-AOF-Tail-File-0.02.tar.gz
    gitpan-cpan-author:       CHENGANG
    gitpan-cpan-maturity:     released

--- lib/Redis/AOF/Tail/File.pm
+++ lib/Redis/AOF/Tail/File.pm
@@ -3,78 +3,113 @@ package Redis::AOF::Tail::File;
 use 5.008008;
 use strict;
 use warnings;
+use File::Tail;
 
-require Exporter;
+our $VERSION = '0.02';
 
-our @ISA = qw(Exporter);
+sub new
+{
+	my $pkg = shift;
+	my $self = {@_};
+	bless $self, $pkg;
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
+	return 0 unless -e $self->{aof_filename};
+	$self->{interval} = 1 unless $self->{interval};
 
-# This allows declaration	use Redis::AOF::Tail::File ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
+	$self->{ARRAY_REDIS_AOF} = ();
+	$self->{FILE_TAIL_FH} = File::Tail->new(
+		name=>$self->{aof_filename}, 
+		interval=>$self->{interval}, 
+		maxinterval=>$self->{interval});
+	return $self;
+}
 
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+sub read_command
+{
+	my $self = shift;
+	return 0 unless $self->{FILE_TAIL_FH};
 
-our @EXPORT = qw(
-	
-);
+	while (defined(my $line = $self->{FILE_TAIL_FH}->read)) 
+	{
+		$line =~ s/\s//g;
+		push @{$self->{ARRAY_REDIS_AOF}}, $line;
+		while( defined ${$self->{ARRAY_REDIS_AOF}}[0] and ${$self->{ARRAY_REDIS_AOF}}[0] !~ /^\*\d/)
+		{
+			shift @{$self->{ARRAY_REDIS_AOF}};
+		}
+		my ($cmd_num) = ${$self->{ARRAY_REDIS_AOF}}[0] =~ /^\*(\d{1,2})/;
 
-our $VERSION = '0.01';
+		next if(scalar @{$self->{ARRAY_REDIS_AOF}} < $cmd_num*2 + 1); # Wait for the complete command
 
-
-# Preloaded methods go here.
+		shift @{$self->{ARRAY_REDIS_AOF}};
+		my $cmd = "";
+		for(1..$cmd_num)
+		{
+			shift @{$self->{ARRAY_REDIS_AOF}};
+			$cmd .= shift @{$self->{ARRAY_REDIS_AOF}};
+			$cmd .= ' ';
+		}
+		$cmd = substr($cmd, 0, -1);
+		return $cmd;
+	}
+}
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-Redis::AOF::Tail::File - Perl extension for blah blah blah
+Redis::AOF::Tail::File - Read redis aof file in realtime
 
 =head1 SYNOPSIS
 
   use Redis::AOF::Tail::File;
-  blah blah blah
+
+	my $aof_file = "/var/redis/appendonly.aof";
+	my $redis_aof = Redis::AOF::Tail::File->new(aof_filename => $aof_file);
+	while (my $cmd = $redis_aof->read_command)
+	{
+  	print "[$cmd]\n";
+	}
 
 =head1 DESCRIPTION
 
-Stub documentation for Redis::AOF::Tail::File, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
+This extension can be used for persistence data from redis to MySQL.
+Maybe you can code like below.
 
-Blah blah blah.
+	use DBI;
+  use Redis::AOF::Tail::File;
+
+	# variables in this comment should be defined
+	# $data_source, $username, $auth, \%attr, 
+	# some_func_translate_redis_command_to_sql()
+	
+  my $dbh = DBI->connect($data_source, $username, $auth, \%attr);
+	my $aof_file = "/var/redis/appendonly.aof";
+	my $redis_aof = Redis::AOF::Tail::File->new(aof_filename => $aof_file);
+	while (my $cmd = $redis_aof->read_command)
+	{
+  	my $sql = some_func_translate_redis_command_to_sql($cmd);
+		$dbh->do($sql);
+	}
+	
 
 =head2 EXPORT
 
 None by default.
 
 
-
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+L<Redis::Term>, L<Redis>
 
 =head1 AUTHOR
 
-陈 钢, E<lt>chengang@apple.comE<gt>
+Chen Gang, E<lt>yikuyiku.com@gmail.comE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2014 by 陈 钢
+Copyright (C) 2014 by Chen Gang
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.16.2 or,
